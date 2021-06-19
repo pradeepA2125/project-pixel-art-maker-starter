58eb7e685961a475e1729eb67e1a2e7e7dd31db9
pradeep kumar <pradeepkumar94p@gmail.com>
2021-06-19 23:25:44 +0530

    dom manipulation added

--- designs.js
+++ designs.js
@@ -1,10 +1,43 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
+// Grid mount function
 function makeGrid() {
 
-// Your code goes here!
+  // Storing grid height value
+  const gridHeight = document.getElementById('inputHeight').value;
+  // Storing grid width value
+  const gridWidth = document.getElementById('inputWidth').value;
+  // Storing table canvas
+  const canvas = document.getElementById('pixelCanvas');
 
+  // Delete rows to start
+  canvas.innerHTML = '';
+
+  // Another way to delete rows
+  // while (canvas.rows.length > 0) {
+  //   canvas.deleteRow(0);
+  // }
+
+  colourChange = function(event) {
+    // When the cell is clicked, the background color changes to the selected color
+    event.target.style.backgroundColor = document.getElementById('colorPicker').value;
+  }
+
+  // Loop to insert the rows
+  for (let i = 0; i < gridHeight; i++) {
+    let row = canvas.insertRow(i);
+
+    // Loop to insert the cells
+    for (let j = 0; j < gridWidth; j++) {
+      let cell = row.insertCell(j);
+
+      // Add click event to the cells
+    }
+  }
+  canvas.addEventListener('click', colourChange);
 }
+
+// Add click event to the submit input
+document.getElementById('sizePicker').addEventListener('submit', function(event) {
+  event.preventDefault();
+  // When the data is submitted the grid mount function is called
+  makeGrid();
+});
